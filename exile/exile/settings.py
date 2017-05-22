fcf944e51c358d3ef2520854dd8b8b488cef5735
mariobp <mariobarrpach@gmail.com>
2017-05-22 17:21:45 -0500

    index

--- exile/exile/settings.py
+++ exile/exile/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'material.theme.cyan',
+    'material',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'supra',
+    'frontend'
 ]
 
 MIDDLEWARE = [
