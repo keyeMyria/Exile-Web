c9229bcda3c24b4d28f57037103b85862c1e123f
mariobp <mariobarrpach@gmail.com>
2017-05-22 17:50:39 -0500

    Cambiar nombre de piscinero a operario, Eliminar modelo de planilla diaria fixed #1

--- exile/usuarios/models.py
+++ exile/usuarios/models.py
@@ -1,6 +1,70 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Usuario(User):
+    identificacion = models.CharField(
+        max_length=120, verbose_name="Identificación", unique=True)
+    fecha_nacimiento = models.DateField(verbose_name="Fecha de nacimiento")
+    direccion = models.CharField(max_length=120, verbose_name="Dirección")
+    telefono = models.CharField(
+        max_length=15, verbose_name="Teléfono", blank=True, null=True)
+    fijo = models.CharField(
+        max_length=15, verbose_name="Fijo", blank=True, null=True)
+    imagen = models.ImageField(upload_to="avatar", null=True, blank=True)
+
+    def avatar(self):
+        if self.imagen:
+            imagen = self.imagen
+        else:
+            imagen = 'account.svg'
+        # end if
+        return '<img class="image-cicle" src="/media/%s" />' % (imagen)
+    # end def
+
+    avatar.allow_tags = True
+
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+    # end def
+# end class
+
+
+class Operario(Usuario):
+    fecha_ingreso = models.DateField(verbose_name="Fecha de Ingreso", blank=True, null=True)
+    fecha_retiro = models.DateField(verbose_name="Fecha de Retiro", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Dibujante"
+        verbose_name_plural = "Dibujantes"
+    # end class
+# end class
+
+
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=100)
+    operarios = models.ManyToManyField(Operario)
+# end class
+
+
+class Supervisor(Usuario):
+    fecha_ingreso = models.DateField(verbose_name="Fecha de Ingreso", blank=True, null=True)
+    fecha_retiro = models.DateField(verbose_name="Fecha de Retiro", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Supervisor"
+        verbose_name_plural = "Supervisores"
+    # end class
+# end class
+
+
+class Asistente(Usuario):
+
+    class Meta:
+        verbose_name = "Asistente"
+        verbose_name_plural = "Asistentes"
+    # end class
+# end class
