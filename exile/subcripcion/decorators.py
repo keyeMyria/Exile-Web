df447d17c1d54105879c07d42b8a9e2db7acf99d
darkdrei <darkdrei88@gmail.com>
2017-06-05 14:30:21 -0700

    creacion del aspecto de limitar el numero de asistentes  #11

--- exile/subcripcion/decorators.py
+++ exile/subcripcion/decorators.py
@@ -29,3 +29,30 @@ def user_plan_operario(view_func):
 
     return _check
 #end def
+
+
+def user_plan_asistente(view_func):
+    def _check(request, *args, **kwargs):
+        if (request.method in ['POST'] and kwargs['pk']) or request.method in ['GET', 'POST', 'PUT'] :
+            if request.user.is_authenticated():
+                cuenta = models.Cuenta.objects.filter(cliente__id=request.user.id).first()
+                if cuenta:
+                    suscripcion = models.Suscripcion.objects.filter(cuenta=cuenta, activa=True, factura__paga=True).first()
+                    if suscripcion:
+                        operario = usuario.Asistente.objects.filter(cuenta=cuenta, eliminado=False).count()
+                        if request.method in ['PUT'] or (request.method in ['POST'] and kwargs['pk']) or (request.method in ['GET'] and suscripcion.plan.operadores > operario) or (suscripcion.plan.operadores >= operario+1 and request.method in ['POST'] and not kwargs['pk']):
+                            return view_func(request, *args, **kwargs)
+                        # end if
+                        return HttpResponse(simplejson.dumps({"error": 'Ya registro los %d asistentes del plan.'%operario}), content_type='application/json', status=403)
+                    #end if
+                    return HttpResponse(simplejson.dumps({"error": "Debe tener una suscripcion activa."}), content_type='application/json', status=403)
+                #end if
+                return HttpResponse(simplejson.dumps({"error": "Debe crear una cuenta."}), content_type='application/json', status=403)
+            #end if
+            return HttpResponse(simplejson.dumps({"error": "Debes iniciar sesion."}), content_type='application/json', status=403)
+        #end if
+        return HttpResponse(simplejson.dumps({"error": "Solicitud incorrecta."}), content_type='application/json', status=403)
+    #end def
+
+    return _check
+#end def
