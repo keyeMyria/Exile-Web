9453f3e0841554acca1dd85f64dcddb41e1dd185
darkdrei <darkdrei88@gmail.com>
2017-06-05 14:27:53 -0700

    finalizacion del aspecto de registro de empleado #11

--- exile/subcripcion/decorators.py
+++ exile/subcripcion/decorators.py
@@ -1,29 +1,30 @@
+import json as simplejson
+from django.shortcuts import redirect, get_object_or_404, HttpResponse
+from usuarios import models as usuario
 import models
-from http import response
-from usuario import models as usuario
 
 
 def user_plan_operario(view_func):
     def _check(request, *args, **kwargs):
-        if request.method in ['POST']:
+        if (request.method in ['POST'] and kwargs['pk']) or request.method in ['GET', 'POST', 'PUT'] :
             if request.user.is_authenticated():
-                cuenta = models.Cuenta.objects.filter(cliente__id=request.id).first()
+                cuenta = models.Cuenta.objects.filter(cliente__id=request.user.id).first()
                 if cuenta:
-                    suscripcion = models.Suscripcion.objects.filter(cuenta=cuenta, activa=True, factura__pagada=True).first()
+                    suscripcion = models.Suscripcion.objects.filter(cuenta=cuenta, activa=True, factura__paga=True).first()
                     if suscripcion:
-                        operario = usuario.Operario.models.filter(cuenta=cuenta, eliminado=False).count()
-                        if suscripcion.plan.operadores < operario:
-                            return funcion(request, *args, **kwargs)
+                        operario = usuario.Empleado.objects.filter(cuenta=cuenta, eliminado=False).count()
+                        if request.method in ['PUT'] or (request.method in ['POST'] and kwargs['pk']) or (request.method in ['GET'] and suscripcion.plan.operadores > operario) or (suscripcion.plan.operadores >= operario+1 and request.method in ['POST'] and not kwargs['pk']):
+                            return view_func(request, *args, **kwargs)
                         # end if
-                        return response(simplejson.dumps({"error": 'Ya registro los %d operarios del plan.'%operario}), 403)
+                        return HttpResponse(simplejson.dumps({"error": 'Ya registro los %d operarios del plan.'%operario}), content_type='application/json', status=403)
                     #end if
-                    return response(simplejson.dumps({"error": "Debe tener una suscripcion activa."}), 403)
+                    return HttpResponse(simplejson.dumps({"error": "Debe tener una suscripcion activa."}), content_type='application/json', status=403)
                 #end if
-                return response(simplejson.dumps({"error": "Debe crear una cuenta."}), 403)
+                return HttpResponse(simplejson.dumps({"error": "Debe crear una cuenta."}), content_type='application/json', status=403)
             #end if
-            return response(simplejson.dumps({"error": "Debes iniciar sesion."}), 403)
+            return HttpResponse(simplejson.dumps({"error": "Debes iniciar sesion."}), content_type='application/json', status=403)
         #end if
-        return response(simplejson.dumps({"error": "Solicitud incorrecta."}), 403)
+        return HttpResponse(simplejson.dumps({"error": "Solicitud incorrecta."}), content_type='application/json', status=403)
     #end def
 
     return _check
