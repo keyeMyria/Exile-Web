b2f3125b7c8f6705e6b9bf217d125c2e7fdf98cb
darkdrei <darkdrei88@gmail.com>
2017-06-05 12:22:08 -0700

    Registro de asistentes y empleados a el admin #11

--- exile/usuarios/admin.py
+++ exile/usuarios/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+import models
 # Register your models here.
+
+admin.register.site(models.Asistente)
+admin.register.site(models.Empleado)
