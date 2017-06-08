c9587541557720c4ff166a3867e7a1b607b4b77b
mariobp <mariobarrpach@gmail.com>
2017-06-08 17:46:02 -0500

    creacion de grupos segun la cuenta #48

--- exile/usuarios/consumers.py
+++ exile/usuarios/consumers.py
@@ -1,26 +1,34 @@
 import json
 from channels import Group
 from channels.auth import channel_session_user, channel_session_user_from_http
+from subcripcion.models import Cuenta
+from django.db.models import Q
 
 
 @channel_session_user_from_http
 def ws_connect(message):
-    Group('users').add(message.reply_channel)
-    print "#######", message.user.username
-    Group('users').send({
-        'text': json.dumps({
-            'username': message.user.username,
-            'is_logged_in': True
-        })
-    })
+    message.reply_channel.send({"accept": True})
+    if message.user:
+        cuenta = Cuenta.objects.filter(Q(cliente=message.user.pk) | Q(usuario=message.user.pk)).first()
+        if cuenta:
+            Group('noti-%d' % cuenta.id).add(message.reply_channel)
+            Group('noti-%d' % cuenta.id).send({
+                'text': json.dumps({
+                    'username': message.user.username,
+                    'is_logged_in': True
+                })
+            })
 
 
 @channel_session_user
 def ws_disconnect(message):
-    Group('users').send({
-        'text': json.dumps({
-            'username': message.user.username,
-            'is_logged_in': False
-        })
-    })
-    Group('users').discard(message.reply_channel)
+    if message.user:
+        cuenta = Cuenta.objects.filter(Q(cliente=message.user.pk) | Q(usuario=message.user.pk)).first()
+        if cuenta:
+            Group('noti-%d' % cuenta.id).send({
+                'text': json.dumps({
+                    'username': message.user.username,
+                    'is_logged_in': False
+                })
+            })
+            Group('noti-%d' % cuenta.id).discard(message.reply_channel)
