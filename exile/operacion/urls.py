3bd959e0bc2a921ebbdc881c28905e72eeee0782
luismoralesp <luismiguel.mopa@gmail.com>
2017-08-10 12:55:44 -0500

    closes #37

--- exile/operacion/urls.py
+++ exile/operacion/urls.py
@@ -8,10 +8,8 @@ import views
 urlpatterns = [
     url(r'^tipo/form/$', views.TipoSupraForm.as_view(), name="tipo"),
     url(r'^tipo/list/$', views.TipoList.as_view(), name="tipo_list"),
-    url(r'^tipo/form/(?P<pk>\d+)/$',
-        views.TipoSupraForm.as_view(), name="tipo_edit"),
-    url(r'^tipo/delete/(?P<pk>\d+)/$',
-        views.TipoDeleteSupra.as_view(), name="tipo_delete"),
+    url(r'^tipo/form/(?P<pk>\d+)/$', views.TipoSupraForm.as_view(), name="tipo_edit"),
+    url(r'^tipo/delete/(?P<pk>\d+)/$', views.TipoDeleteSupra.as_view(), name="tipo_delete"),
 ]
 
 
@@ -22,10 +20,8 @@ urlpatterns = [
 urlpatterns += [
     url(r'^cliente/form/$', views.ClienteSupraForm.as_view(), name="cliente"),
     url(r'^cliente/list/$', views.ClienteList.as_view(), name="cliente_list"),
-    url(r'^cliente/form/(?P<pk>\d+)/$',
-        views.ClienteSupraForm.as_view(), name="cliente_edit"),
-    url(r'^cliente/delete/(?P<pk>\d+)/$',
-        views.ClienteDeleteSupra.as_view(), name="cliente_delete"),
+    url(r'^cliente/form/(?P<pk>\d+)/$', views.ClienteSupraForm.as_view(), name="cliente_edit"),
+    url(r'^cliente/delete/(?P<pk>\d+)/$', views.ClienteDeleteSupra.as_view(), name="cliente_delete"),
 ]
 
 
@@ -37,10 +33,8 @@ urlpatterns += [
 urlpatterns += [
     url(r'^lugar/form/$', views.LugarSupraForm.as_view(), name="lugar"),
     url(r'^lugar/list/$', views.LugarList.as_view(), name="lugar_list"),
-    url(r'^lugar/form/(?P<pk>\d+)/$',
-        views.LugarSupraForm.as_view(), name="lugar_edit"),
-    url(r'^lugar/delete/(?P<pk>\d+)/$',
-        views.LugarDeleteSupra.as_view(), name="lugar_delete"),
+    url(r'^lugar/form/(?P<pk>\d+)/$', views.LugarSupraForm.as_view(), name="lugar_edit"),
+    url(r'^lugar/delete/(?P<pk>\d+)/$', views.LugarDeleteSupra.as_view(), name="lugar_delete"),
 ]
 
 
