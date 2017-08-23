0d33d477a111056c7f0dde2bb61b04ca0511f843
mariobp <mariobarrpach@gmail.com>
2017-08-23 18:50:43 -0500

    routing

--- exile/exile/routing.py
+++ exile/exile/routing.py
@@ -1,7 +1,7 @@
 from channels import include
 
 channel_routing = [
-    include("usuarios.routing.websocket_routing", path=r"^/es/(?P<pk>\d+)/"),
+    include("usuarios.routing.websocket_routing", path=r"^/noti/(?P<pk>\d+)/"),
     #route_class(UsuariosDemultiplexerBiding, path="^/binding/usuarios/(?P<pk>\d+)/"),
     #route_class(NovedadesDemultiplexerBiding, path="^/binding/novedades/(?P<pk>\d+)/"),
     #route_class(OperacionDemultiplexerBiding, path="^/binding/operacion/(?P<pk>\d+)/"),
